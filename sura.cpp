fcdb59ee046c2e80a7837792ae72f8657be4d5ef
Randy Carlson <ryancarlson486@gmail.com>
2016-04-02 19:51:43 -0400

    Added basic skeleton code

--- sura.cpp
+++ sura.cpp
@@ -3,9 +3,61 @@
 using std::cin;
 using std::cout;
 using std::endl;
+using std::string;
 
-int main() {
-    cout << "ayyyyyyy" << endl << "lmao" << endl;
+enum Input { QUIT, NOTHING };
+
+Input latestInput;
+
+struct character
+{
+    int strength = 10;
+    int agility = 10;
+    int fortitude = 10;
+    int health;
+} player;
+
+int getMaxHealth(character c)
+{
+    return c.fortitude;
+}
+
+void setup()
+{
+    player.health = getMaxHealth(player);
+    cout << "Welcome to Sura!" << endl;
+}
+
+void mainLoop()
+{
+    switch(latestInput)
+    {
+        case NOTHING:
+            cout << "You did nothing" << endl;
+            break;
+        default:
+            std::cerr << "invalid Input" << endl;
+            break;
+    }
+}
+
+
+Input getInput()
+{
+    cout << "What is your action this turn? ";
+    string input;
+    std::getline(cin, input);
+    latestInput = NOTHING;
+    return NOTHING;
+}
+
+int main()
+{
+    setup();
+    do
+    {
+        mainLoop();
+    } while(getInput() != QUIT);
     
     return 0;
 }
